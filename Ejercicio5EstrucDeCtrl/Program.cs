0d5016e8390439e214a4e5a96f64523f1d0c3e5a
S. Ulises Ferreyra <ulises.ferreyra@hotmail.com>
2019-04-17 15:35:08 -0300

    Hecho hasta promedio

--- Ejercicio5EstrucDeCtrl/Program.cs
+++ Ejercicio5EstrucDeCtrl/Program.cs
@@ -10,6 +10,114 @@ namespace Ejercicio5EstrucDeCtrl
     {
         static void Main(string[] args)
         {
+            /* string ingreso;
+             bool ok = false;
+
+              while (!ok)
+
+              { Console.WriteLine("Ingrese un numero mayor a 0");
+                  ingreso = Console.ReadLine();
+                  ok = int.TryParse(ingreso, out int entero);
+
+                  for (int n = 0; n <= entero; n++)
+
+                  { Console.WriteLine(n); }   };
+
+             Console.ReadKey(); 
+
+             string ingreso;
+             bool ok = false;
+
+             while (!ok)
+
+             {
+                 Console.WriteLine("Ingrese un numero mayor a 0");
+                 ingreso = Console.ReadLine();
+                 ok = int.TryParse(ingreso, out int entero);
+
+                 Console.WriteLine($"Ingrese un número entre 0 y {entero}");
+                 ingreso = Console.ReadLine();
+
+                 ok = int.TryParse(ingreso, out int m);
+
+                 if (m < entero && m > 0)
+
+                 for (int n = 0; n <= entero; n= n+m)
+
+                 { Console.WriteLine(n); } 
+             }; 
+
+             Console.ReadKey(); 
+              string ingreso;
+             bool ok = false;
+
+             do
+             {
+                 Console.WriteLine("Ingrese un numero mayor a 0");
+                 ingreso = Console.ReadLine();
+                 ok = int.TryParse(ingreso, out int entero);
+
+                 Console.WriteLine($"Ingrese un número entre 0 y {entero}");
+                 ingreso = Console.ReadLine();
+
+                 ok = int.TryParse(ingreso, out int m);
+
+                 if (m < entero && m > 0)
+
+                     for (int n = 0; n <= entero; n = n + m)
+
+                     { Console.WriteLine(n); }
+             }
+             while (!ok);
+
+             Console.ReadKey();  */
+
+             decimal[] numero = new decimal[20];
+            decimal suma;
+            decimal promedio;
+
+            for (var i = 0; i < 20; i++)
+             {
+
+                 decimal valor;
+                 string ingreso;
+
+                 bool ok = false;
+
+                while (!ok)
+
+                {
+                    Console.WriteLine("Ingrese un número (Ingreso máximo: 20)");
+
+                    ingreso = Console.ReadLine();
+                    ok = decimal.TryParse(ingreso, out valor);
+
+                    numero[i] = valor;
+
+
+                    suma = 0;
+
+                    suma = suma + numero[i];
+                    promedio = suma / 20;
+
+                    {Console.WriteLine("Promedio de numeros ingresados:" +promedio); };
+
+                }
+            }
+            Console.ReadKey(); 
+
+            
+            
+
+
+
+
+
+
+
+
         }
+
     }
-}
+    }
+
