d8c8a353ae70c35bbef0a771ac10bd973cdf1228
S. Ulises Ferreyra <ulises.ferreyra@hotmail.com>
2019-04-20 04:09:40 -0300

    Hasta calculadora

--- Ejercicio5EstrucDeCtrl/Program.cs
+++ Ejercicio5EstrucDeCtrl/Program.cs
@@ -70,9 +70,9 @@ namespace Ejercicio5EstrucDeCtrl
              }
              while (!ok);
 
-             Console.ReadKey();  */
+             Console.ReadKey();  
 
-            decimal[] numero = new decimal[4];
+            decimal[] numero = new decimal[20];
             decimal suma = 0;            
 
             Console.WriteLine("Ingrese un número");
@@ -93,8 +93,80 @@ namespace Ejercicio5EstrucDeCtrl
             }
             decimal promedio = suma / numero.Length;            
 
-            Console.WriteLine($"Promedio de numeros ingresados: {promedio} ");
+            Console.WriteLine($"[{suma}] / [{numero.Length}] = [{promedio}] ");
+            Console.ReadKey(); */
+
+            List<decimal> numero = new List<decimal>();
+            decimal suma = 0;
+            bool ingresoX = false;
+            
+            string ingreso;
+
+            decimal valor;
+
+            decimal promedio = 0;
+
+            while (!ingresoX)
+            {
+                bool ok = false;
+                Console.WriteLine("Ingrese una secuencia de números. Para terminar escriba una X");
+                ingreso = Console.ReadLine();
+
+                while (!ok)
+                {
+                    if (ingreso == "x" || ingreso == "X"   || string.IsNullOrWhiteSpace(ingreso)) 
+                        ingresoX = true;
+                    break;
+                };
+
+                
+                ok = decimal.TryParse(ingreso, out valor);
+
+                if(ingresoX == false)
+                    
+
+                numero.Add(valor);
+            
+            };
+            foreach (decimal nro in numero)
+            { suma += nro;
+              promedio = suma / (numero.Count);
+            };
+
+            Console.WriteLine($"[{suma}] / [{(numero.Count)}] = {promedio}");
+
+            Console.WriteLine("MAX:"+numero.Max());
+            Console.WriteLine("MIN:" + numero.Min());
+            Console.WriteLine("El primero ingresado fue:" + numero.First());
+            Console.WriteLine("El úlitmo ingresado fue:" + numero.Last());
+
+            numero.Sort();
+            Console.WriteLine("La lista ordenadad de forma ascendente");
+            foreach (decimal nro in numero)
+                Console.WriteLine(nro);
+            numero.Reverse();
+
+            Console.WriteLine("La lista ordenadad de forma descendente");
+            foreach (decimal nro in numero)
+                Console.WriteLine(nro);
+
             Console.ReadKey();
+
+
+      
+            
+     
+        
+
+            
+
+
+
+        
+      
+            
+
+       
 
 
         }
